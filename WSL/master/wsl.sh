4804bcae1c16417dca850a4ef44b867b48fd5e63
José M. Taveras <josemtaverasreyes@gmail.com>
2024-08-15 18:29:05 -0400

    add(/master/wsl.sh):a variable that stores the windows user
    create a variable that stores the current windows user

--- WSL/master/wsl.sh
+++ WSL/master/wsl.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Definición de colores para mensajes de error
-COLOR_ERROR="\033[31m"  # Rojo para mensajes de error
-COLOR_RESET="\033[0m"   # Restablece el color al valor predeterminado
+COLOR_ERROR="\033[31m"  
+COLOR_RESET="\033[0m"
+windows_username=$(wine cmd /c echo %USERNAME%)
+WINDOWS_USER_PATH="/mnt/c/Users/$windows_username"
 
 
 # Función para abrir un archivo o directorio en Vim
@@ -12,12 +14,12 @@ vimt() {
         return 1
     fi
 
-    local target="/mnt/c/Users/$windowsuser/$1"
+    local target="/mnt/c/Users/$windows_username/$1"
 
     if [ -e "$target" ]; then
         vim "$target"  # Abre el archivo en Vim
     else
-        echo -e "${COLOR_ERROR}Error: El archivo o directorio \"$1\" no existe en /mnt/c/Users/$windowsUser/.${COLOR_RESET}"
+        echo -e "${COLOR_ERROR}Error: El archivo o directorio \"$1\" no existe en /mnt/c/Users//.${COLOR_RESET}"
         return 1
     fi
 }
@@ -28,7 +30,7 @@ touchw() {
         return 1
     fi
 
-    touch "/mnt/c/Users/$windowsuser/$@"
+    touch "/mnt/c/Users/$windows_username/$@"
 }
 
 # Función para copiar archivos del sistema de archivos de Windows a WSL
@@ -38,7 +40,7 @@ cpw() {
         return 1
     fi
 
-    local source="/mnt/c/Users/$windowsuser/$1"
+    local source="/mnt/c/Users/$windows_username/$1"
     local destination="/home/$USER/"
 
     cp -rfu "$source" "$destination"
@@ -50,25 +52,25 @@ rmall() {
         return 1
     fi
 
-    rm -rf "/mnt/c/Users/$windowsuser/$1"
+    rm -rf "/mnt/c/Users/$windows_username/$1"
 }
 
 lsdw() {
-    if [ -z "$windowsuser" ]; then
+    if [ -z "$windows_username" ]; then
         echo -e "${COLOR_ERROR}Error: Se requiere especificar el usuario.\033[0m"
         return 1
     fi
 
-    ls -a "/mnt/c/Users/$windowsuser"
+    ls -a "/mnt/c/Users/$windows_username"
 }
 
 du-wd() {
     if [ -z "$1" ]; then
         # No se proporciona argumento, usa el directorio predeterminado
-        local dir="/mnt/c/Users/$windowsuser/"
+        local dir="/mnt/c/Users/$windows_username/"
     else
         # Se proporciona argumento, usa el directorio especificado
-        local dir="/mnt/c/Users/$windowsuser/$1"
+        local dir="/mnt/c/Users/$windows_username/$1"
     fi
     
     # Verifica si el directorio existe antes de ejecutar `du`
