004a5e7610dbb74dc31600f66b668257adb55909
José M. Taveras <josemtaverasreyes@gmail.com>
2024-08-19 17:32:24 -0400

    test(Format): test of github actions

---
+++ WSL/test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ echo "Hola, mundo!" for i in {1..5}; do echo "Número: $i"; done echo "Fin del script"if [ -f /etc/passwd ]; then echo "El archivo existe";else echo "El archivo no existe";fi echo "Proceso terminado"
