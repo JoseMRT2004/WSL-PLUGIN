f7ffe38427c4e9301a0ccb43d862ed1e94f4847f
José M. Taveras <josemtaverasreyes@gmail.com>
2024-08-19 23:20:11 -0400

    change(Formatter/): formatter the code onside files .sh
    ajustar y organizar el código de manera que sea más fácil de leer y entender, facilitando su mantenimiento y colaboración.

--- WSL/test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
- echo "Hola, mundo!" for i in {1..5}; do echo "Número: $i"; done echo "Fin del script"if [ -f /etc/passwd ]; then echo "El archivo existe";else echo "El archivo no existe";fi echo "Proceso terminado"
